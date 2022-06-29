6110374acfd1b58a2bcd6937f339af4da9060e0c
Koen Derks <koen-derks@hotmail.com>
2022-06-29 21:45:55 +0200

    Update action.R

--- .github/workflows/action.R
+++ .github/workflows/action.R
@@ -2,7 +2,7 @@ cities <- subset(rcityviews::cities, rcityviews::cities$population > 100000)
 
 city <- cities[sample(1:nrow(cities), size = 1), ]
 border <- sample(x = c("circle", "rhombus", "hexagon", "octagon", "decagon"), size = 1)
-theme <- sample(x = c("original", "light", "dark", "destination", "rouge", "colored", "neon"), size = 1)
+theme <- sample(x = c("original", "light", "dark", "rouge", "verde", "colored", "neon"), size = 1)
 
 sink(file = "png/name.txt")
 p <- rcityviews::cityview(name = city[["name"]], theme = theme, border = border,
