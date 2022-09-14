e6770b2e8cb70cb8f0f9538d4ec9f2bedeb0de53
Koen Derks <koen-derks@hotmail.com>
2022-09-14 12:58:52 +0200

    Update action.R

--- .github/workflows/action.R
+++ .github/workflows/action.R
@@ -7,7 +7,7 @@ p <- rcityviews::cityview(name = city[["name"]],
                           theme = sample(x = c("original", "light", "dark", "rouge", "verde", "colored", "neon", "atlantis", "vintage", "lichtenstein"), size = 1),
                           border = sample(x = c("circle", "rhombus", "hexagon", "octagon", "decagon"), size = 1),
                           halftone = sample(x = c("none", "light", "dark"), size = 1, prob = c(0.5, 0.25, 0.25)),
-                          places = 10,
+                          places = 8,
                           filename = "png/daily.png",
                           verbose = FALSE,
                           bot = TRUE)
