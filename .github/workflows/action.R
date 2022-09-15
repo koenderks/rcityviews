a6e0583e2815a144aa493232caf0a66cba3424c7
Koen Derks <koen-derks@hotmail.com>
2022-09-15 22:35:18 +0200

    Update action.R

--- .github/workflows/action.R
+++ .github/workflows/action.R
@@ -1,6 +1,6 @@
 sink(file = "png/name.txt")
 p <- rcityviews::cityview(name = NULL, # Take a random city
-                          zoom = sample(x = c(1, 0.5, 0.25), size = 1, prob = c(0.5, 0.25, 0.25)),
+                          zoom = sample(x = c(1, 0.75, 0.5, 0.25), size = 1),
                           theme = sample(x = c("original", "light", "dark", "rouge", "verde", "colored", "neon", "atlantis", "vintage", "lichtenstein"), size = 1),
                           border = sample(x = c("circle", "rhombus", "hexagon", "octagon", "decagon"), size = 1),
                           halftone = sample(x = c("none", "light", "dark"), size = 1, prob = c(0.5, 0.25, 0.25)),
