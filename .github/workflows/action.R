740a2cdc22d86a5c6a7ade4b09ec21aa3010e1c9
Koen Derks <koen-derks@hotmail.com>
2022-09-19 13:44:08 +0200

    Update

--- .github/workflows/action.R
+++ .github/workflows/action.R
@@ -1,6 +1,7 @@
 sink(file = "png/name.txt")
 p <- rcityviews::cityview(name = NULL, # Take a random city
                           theme = sample(x = c("rouge", "colored", "delftware", "vintage"), size = 1),
+                          halftone = "light",
                           filename = "png/daily.png",
                           verbose = FALSE,
                           bot = TRUE)
