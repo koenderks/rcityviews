4073427edf4585c0752921acdfea2c8824219f9a
Koen Derks <koen-derks@hotmail.com>
2023-04-09 22:28:30 +0200

    Update font name

--- R/zzz.R
+++ R/zzz.R
@@ -22,8 +22,8 @@
   sysfonts::font_add_google("Fredericka the Great")
   sysfonts::font_add_google("Dancing Script")
   sysfonts::font_add_google("Walter Turncoat")
-  sysfonts::font_add("American Uncial Regular", file.path("inst", "fonts", "American Uncial Regular.otf"))
-  sysfonts::font_add("Rage", file.path("inst", "fonts", "Rage.otf"))
+  sysfonts::font_add("American Uncial Regular", file.path("inst", "fonts", "uncial.otf"))
+  sysfonts::font_add("Rage", file.path("inst", "fonts", "rage.otf"))
   showtext::showtext_auto()
   osmdata::set_overpass_url("https://overpass.kumi.systems/api/interpreter")
 }
